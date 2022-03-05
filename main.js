9f46570ce91d13e842e95ed61c8017e591e46deb
NateRowsell <nsr.72@hotmail.com>
2022-03-04 20:54:01 -0400

    generating cards

--- main.js
+++ main.js
@@ -17,6 +17,49 @@ async function getAllJobs(array) {
   return allJobs
 }
 
+function createDiv(id) {
+  let div = document.createElement('div')
+  div.setAttribute('id', id)
+  return div
+}
+
+function addJobSkills() {
+  // for loop that appends job skills to #jobSkills
+  let jobSkillsContainer = document.getElementById('jobSkills')
+
+  //for i append div with class of .jobSkill
+}
+
 function createJobDivs(allJobs) {
   console.log(allJobs[1])
+
+  //below must go in for loop array length
+
+  const jobsContainer = document.getElementById('jobsContainer')
+  let jobContainer = createDiv('jobContainer')
+  let jobLogo = createDiv('jobLogo')
+  let companyAndTag = createDiv('companyAndTag')
+  let jobTitle = createDiv('jobTitle')
+  let jobCompany = createDiv('jobCompany')
+  let jobNew = createDiv('jobNew')
+  let jobPostedTimeLocation = createDiv('jobPostedTimeLocation')
+  let jobPosted = createDiv('jobPosted')
+  let jobTime = createDiv('jobTime')
+  let jobLocation = createDiv('jobLocation')
+
+  jobsContainer.appendChild(jobContainer)
+
+  jobContainer.appendChild(jobLogo)
+
+  jobContainer.appendChild(companyAndTag)
+
+  companyAndTag.append(jobCompany, jobNew)
+
+  jobContainer.appendChild(jobTitle)
+
+  jobContainer.appendChild(jobPostedTimeLocation)
+
+  jobPostedTimeLocation.append(jobPosted, jobTime, jobLocation)
+
+  // add my content from array here
 }
